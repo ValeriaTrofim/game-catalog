0ba8cc2f19cb3684b27f2eba2f5171fe241e1650
Valeria Trofim <valeria.trofim17@gmail.com>
2023-09-08 16:35:04 +0200

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import "./App.css";
-import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container>
+        <Grid item xs={16}>
+          <Item>nav</Item>
+        </Grid>
+        <Grid item xs={6} sx={{ display: { xs: "none", sm: "block" } }}>
+          <Item>Aside</Item>
+        </Grid>
+        <Grid item xs={16} sm={6} md={6}>
+          <Item>main</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default App;
