6fd5aa2e5af96a14304400f6bdb0c1847606268b
Valeria Trofim <valeria.trofim17@gmail.com>
2023-09-14 17:41:04 +0200

    Display NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,8 @@ import Stack from "@mui/material/Stack";
 import AppBar from "@mui/material/AppBar";
 import IconButton from "@mui/material/IconButton";
 import logo from "../assets/logo.webp";
+import Typography from "@mui/material/Typography";
+
 // import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 // const darkTheme = createTheme({
@@ -17,7 +19,13 @@ const NavBar = () => {
   return (
     <Stack>
       {/* <ThemeProvider theme={darkTheme}> */}
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        sx={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
         <IconButton
           color="inherit"
           aria-label="menu"
@@ -25,6 +33,9 @@ const NavBar = () => {
         >
           <img src={logo} width={60} height={60} />
         </IconButton>
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+          NavBar
+        </Typography>
       </AppBar>
       {/* </ThemeProvider> */}
     </Stack>
